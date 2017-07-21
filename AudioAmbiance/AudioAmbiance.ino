7c304e0861fa70fd22e6f0be4f372f7bebcd52f4
Jordan Guzak <>
2017-07-20 17:41:03 -0700

    set global variables and constants

--- AudioAmbiance/AudioAmbiance.ino
+++ AudioAmbiance/AudioAmbiance.ino
@@ -1,33 +1,56 @@
 // Author: Jordan Guzak
 // Audio Ambiance is designed to enhance your listening experience
 
+#include <Adafruit_NeoPixel.h>
 #include "ThreewaySwitch.h"
 #include "Potentiometer.h"
+#include "MonoMode.h"
 
+// pin constants:
+int leftStripPin = 10;
+int rightStripPin = 11;
+int numPixels = 60;
+
+// internal state variables
 Switch_Position curMode, checkMode;
+
+// hardware objects
 ThreewaySwitch modeSwitch(3, 4, 5);
-
-
 Potentiometer knob1(A0), knob2(A1), knob3(A2);
 
+Adafruit_NeoPixel leftStrip = Adafruit_NeoPixel(numPixels, leftStripPin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel rightStrip = Adafruit_NeoPixel(numPixels, rightStripPin, NEO_GRB + NEO_KHZ800);
+
 void setup() {
-  // initial potentiometer and switch read
-  curMode = modeSwitch.checkPosition();
+    // initial potentiometer and switch read
+    curMode = modeSwitch.checkPosition();
+
+    // clear led strips
+    leftStrip.begin();
+    rightStrip.begin();
+    leftStrip.show();
+    rightStrip.show();
 }
 
 void loop() {
-  checkMode = modeSwitch.checkPosition();
-  if (curMode != checkMode) {
-    curMode = checkMode;
-  }
+    checkMode = modeSwitch.checkPosition();
+    if (curMode != checkMode) {
+        curMode = checkMode;
+    }
 
-  switch(curMode) {
+    switch(curMode) {
     case UP:
-      break;
+        // mono mode
+        
+        break;
     case CENTER:
-      break;
+        // chroma mode
+        
+        break;
     case DOWN:
-      break;
-  }
+        // basic fft mode
+
+        break;
+    }
 
 }
---
+++ AudioAmbiance/MonoMode.h
@@ -0,0 +1,6 @@
+// Author: Jordan Guzak
+
+void monoVolumeResponse(Adafruit_Neopixel strip, int numPixels, int volume, int[] color) {
+
+    strip.show();
+}
